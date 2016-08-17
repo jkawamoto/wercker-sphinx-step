7cf4d5a6faad39cef68921fdb0fc0570ea0bf213
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-08-17 00:10:47 -0400

    Update run.sh.

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 # /bin/bash
+if [ -n "$WERCKER_SPHINX_BASEDIR" ]; then
+  WERCKER_SPHINX_OPTIONS = $WERCKER_SPHINX_OPTIONS" --directory "$WERCKER_SPHINX_BASEDIR
+fi
+
 echo "Installing Sphinx and other packages."
 pip install --upgrade sphinx $WERCKER_SPHINX_PACKAGES
 
 if [ -e "requirements.txt" ]; then
   echo "Installing required packages."
   pip install -r requirements.txt
-fi
-
-if [ -n "$WERCKER_SPHINX_BASEDIR" ]; then
-  WERCKER_SPHINX_OPTIONS = $WERCKER_SPHINX_OPTIONS" --directory "$WERCKER_SPHINX_BASEDIR
 fi
 
 echo "Compiling the document."
