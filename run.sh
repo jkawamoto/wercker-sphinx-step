d97c63225c711fdc12897023aa923d776fc13061
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-12-03 13:59:37 -0500

    Use the stable version of Sphinx.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [ -n "$WERCKER_SPHINX_BASEDIR" ]; then
 fi
 
 echo "Installing Sphinx and other packages."
-pip install --upgrade sphinx $WERCKER_SPHINX_PACKAGES
+pip install --upgrade -e git+https://github.com/sphinx-doc/sphinx@stable#egg=sphinx-stable $WERCKER_SPHINX_PACKAGES
 
 if [ -e "requirements.txt" ]; then
   echo "Installing required packages."
