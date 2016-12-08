4a02e1ae436b8d580b624608904f046ee3a093db
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-12-07 22:05:26 -0500

    Use logging function in run.sh.

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@ if [ -n "$WERCKER_SPHINX_BASEDIR" ]; then
   WERCKER_SPHINX_OPTIONS=$WERCKER_SPHINX_OPTIONS" --directory "${WERCKER_SPHINX_BASEDIR}
 fi
 
-echo "Installing Sphinx and other packages."
+info "Installing Sphinx and other packages."
 pip install --upgrade -e git+https://github.com/sphinx-doc/sphinx@stable#egg=sphinx-stable $WERCKER_SPHINX_PACKAGES
 
 if [ -e "requirements.txt" ]; then
-  echo "Installing required packages."
+  info "Installing required packages."
   pip install -r requirements.txt
 fi
 
-echo "Compiling the document."
+info "Compiling the document."
 make $WERCKER_SPHINX_TARGET $WERCKER_SPHINX_OPTIONS
